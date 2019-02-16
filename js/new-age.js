26851374c772fef8e110f3f9bdb9d6acdcce570d
bpeter97 <brian.lee.peter@gmail.com>
2019-02-15 17:50:23 -0800

    Add logo and redesign navbar

--- js/new-age.js
+++ js/new-age.js
@@ -3,35 +3,45 @@
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 48
+          },
+          1000,
+          "easeInOutExpo"
+        );
         return false;
       }
     }
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
+  $("body").scrollspy({
+    target: "#mainNav",
     offset: 54
   });
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
+    if ($("#mainNav").offset().top > 200) {
       $("#mainNav").addClass("navbar-shrink");
+      $("#mainNav").addClass("navbar-slide-up");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
+      $("#mainNav").removeClass("navbar-slide-up");
     }
   };
   // Collapse now if page is not at top
@@ -39,4 +49,23 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+  var masthead = document.querySelector(".masthead");
+
+  function setTranslate(xPos, yPos, el) {
+    el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+  }
+
+  window.addEventListener("DOMContentLoaded", scrollLoop, false);
+
+  var xScrollPosition;
+  var yScrollPosition;
+
+  function scrollLoop() {
+    xScrollPosition = window.scrollX;
+    yScrollPosition = window.scrollY;
+
+    setTranslate(0, yScrollPosition * -0.1, masthead);
+
+    requestAnimationFrame(scrollLoop);
+  }
 })(jQuery); // End of use strict
